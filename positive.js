79523fcc9bd5644623d2943564366f8703728b6e
Udon-japanese <108126644+Udon-japanese@users.noreply.github.com>
2022-08-07 21:54:47 +0900

    説明の追加・コードの改良

--- positive.js
+++ positive.js
@@ -1,31 +1,22 @@
-// if(18 = 81){
-// location.replace("https://worddrow.net/searchReverse?keyword=%E5%AB%8C");
-// }else{
-
-// }
-
-
 'use strict';
-const negativeInput = document.getElementById('negative-word')
-const positiveButton = document.getElementById('positive-button')
+const negativeInput = document.getElementById('negative-word');
+const positiveButton = document.getElementById('positive-button');
 
 negativeInput.onkeydown = event => {
     if(event.key === 'Enter'){
         positiveButton.onclick();
     }
-}
+}//エンターキーを押しても実行できるように
 
 positiveButton.onclick = () => {
     const negativeWord = negativeInput.value;
     if (negativeWord.length === 0) {
         return;
     }
-    console.log(negativeWord);
-    negativeMakeOver()
-};
+    negativeMakeOver();
+};//何も入力されない場合は実行しない。単語が入力された場合は関数negativeMakeOver();を実行する。
 
 function negativeMakeOver(){
     const negativeEncoded = negativeInput.value;
-    window.open(`https://worddrow.net/searchReverse?keyword=${encodeURIComponent(negativeEncoded)}`)
-}
-
+    window.open(`https://worddrow.net/searchReverse?keyword=${encodeURIComponent(negativeEncoded)}`);
+}//encodeURIComponent()で、入力された単語をパーセントエンコーディングし、それをURLの後ろに付け足す。何度も違う単語で試せるよう、別タブで開くようにした。
